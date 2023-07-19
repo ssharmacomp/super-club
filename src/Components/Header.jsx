3f2bba4f9a66fcfa06c275a04d85e311fbfe2167
shyam-sharma <shyam.sharma@bp.liferay.com>
2023-07-19 22:03:25 +0530

    set active state on navigation

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,34 +1,40 @@
 import {useState,} from "react"; 
 import { HStack } from "@chakra-ui/react";
 import Logo from"../Images/logo.png";
-
+import logoWhite from"../Images/logo-white.png";
 
 const Header = () =>{
-  var [curNavId, setCurNavId] = useState("home"); 
+  var [curNavId, setCurNavId] = useState("landing-nav"); 
+  var[logoSrc, setLogoSrc] = useState(Logo);
   
   const handleClick = (anchor) => () => {
-    console.log(curNavId);
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
+    var activeNav = document.getElementById(curNavId);
+    activeNav.classList.remove("active");
+    var clickedNavID = `${anchor}-nav`;
+    var clickedNav = document.getElementById(clickedNavID);
+    activeNav.classList.remove("active");
+    clickedNav.classList.add("active");
+    setCurNavId(clickedNavID);
     if (element) {
       element.scrollIntoView({
         behavior: "smooth",
         block: "start"
       });
-      
     }
-      
   };
 
   return(
     <HStack className="header container" display={"block"}>
-        <img src={Logo} alt="super club"/>
+        
+        <img src={logoSrc} alt="super club"/>
         <nav className="scrolling-navigation">
             <HStack spacing={8} className="nav-wrap">
-              <a id="home" href="#landing" onClick={handleClick("landing")} className="active link">Home</a>
-              <a id="prodFeatures" href="#features" onClick={handleClick("features")} className="link">Projects</a>
-              <a id="ourLocations" href="#locations" onClick={handleClick("locations")} className="link">Locations</a>
-              <a id="contactMe" href="#contact" onClick={handleClick("contact")} className="link">Contact</a>
+              <a id="landing-nav" href="#landing" onClick={handleClick("landing")} className="active link">Home</a>
+              <a id="features-nav" href="#features" onClick={handleClick("features")} className="link">Projects</a>
+              <a id="locations-nav" href="#locations" onClick={handleClick("locations")} className="link">Locations</a>
+              <a id="contact-nav" href="#contact" onClick={handleClick("contact")} className="link">Contact</a>
             </HStack>
           </nav>
     </HStack>
